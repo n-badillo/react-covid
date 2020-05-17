97a8ea9815caa50d04df9567bdfc731e95d50b67
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-05-16 17:59:56 -0700

    Updates

---
+++ precache-manifest.618f8a06619fa055c91f00f5e37330e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1c9d9f379b8aa83bec56e6c18789e1",
+    "url": "/react-covid/index.html"
+  },
+  {
+    "revision": "64e3b378596fe069fd2c",
+    "url": "/react-covid/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "64e3b378596fe069fd2c",
+    "url": "/react-covid/static/js/2.4cc7a2ee.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/react-covid/static/js/2.4cc7a2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7f4aa682f8ea53abc0",
+    "url": "/react-covid/static/js/main.4d3ca149.chunk.js"
+  },
+  {
+    "revision": "ec0fe170a72d0bb1fe7a",
+    "url": "/react-covid/static/js/runtime-main.a248a32c.js"
+  },
+  {
+    "revision": "acf96ebaed247deb3fddb6264dfa89f2",
+    "url": "/react-covid/static/media/fadingheart.acf96eba.png"
+  }
+]);
