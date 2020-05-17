61cbd046c1198455ed3bbcd9c037d5d044afd4b0
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-05-16 23:26:10 -0700

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,12 @@ const About = () => {
       This website was created as a proof of concept for CPSC 349 - Front End Development.  I plan to implement a couple features to this website in the future (maybe after the project deadline) to increase the project's functionality:
       <ul><li>Add the ability to choose a different country that is not the United States</li>
       <li>Have the ability to enable state and city data using another API</li>
-      <li>Implement Choropleth maps or other forms of data visualization</li></ul>
+      <li>Implement Choropleth maps or other forms of data visualization</li>
       <li>Add a "Region" page, where you could get average stats of all countries located in one region (ie, North America would return Mexico, Canada and United States data and how they compare)</li>
-      
+      </ul>
       </div>
+      <div><h4>References</h4></div>
+      <div>The API is located <a href='https://api.covid19api.com/total/country/united-states'>here.</a></div>
       </div>
   )
 }
